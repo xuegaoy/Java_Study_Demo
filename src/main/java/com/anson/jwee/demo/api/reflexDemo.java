e76c571ff0816c58d3658927149ffff2489e0ef1
anson <xuegaoyuan@aliyun.com>
2018-09-28 11:17:08 +0800

    init commit

--- src/main/java/com/anson/jwee/demo/api/reflexDemo.java
+++ src/main/java/com/anson/jwee/demo/api/reflexDemo.java
@@ -1,4 +1,4 @@
-package com.reflex.demo;
+package com.anson.jwee.demo.api;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
@@ -6,9 +6,10 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
-public class Main {
+public class reflexDemo {
     /**
      * 为了看清楚Java反射部分代码，所有异常我都最后抛出来给虚拟机处理！
+     *
      * @param args
      * @throws ClassNotFoundException
      * @throws InstantiationException
@@ -59,20 +60,19 @@ public class Main {
     /**
      * Demo1: 通过Java反射机制得到类的包名和类名
      */
-    public static void Demo1()
-    {
+    public static void Demo1() {
         Person person = new Person();
-        System.out.println("Demo1: 包名: " +"--"+person.getClass()+"--"+person.getClass().getPackage()+"--"
+        System.out.println("Demo1: 包名: " + "--" + person.getClass() + "--" + person.getClass().getPackage() + "--"
                 + person.getClass().getPackage().getName() + "，"
                 + "完整类名: " + person.getClass().getName());
     }
 
     /**
      * Demo2: 验证所有的类都是Class类的实例对象
+     *
      * @throws ClassNotFoundException
      */
-    public static void Demo2() throws ClassNotFoundException
-    {
+    public static void Demo2() throws ClassNotFoundException {
         //定义两个类型都未知的Class , 设置初值为null, 看看如何给它们赋值成Person类
         Class<?> class1 = null;
         Class<?> class2 = null;
@@ -90,12 +90,12 @@ public class Main {
 
     /**
      * Demo3: 通过Java反射机制，用Class 创建类对象[这也就是反射存在的意义所在]
+     *
      * @throws ClassNotFoundException
      * @throws IllegalAccessException
      * @throws InstantiationException
      */
-    public static void Demo3() throws ClassNotFoundException, InstantiationException, IllegalAccessException
-    {
+    public static void Demo3() throws ClassNotFoundException, InstantiationException, IllegalAccessException {
         Class<?> class1 = null;
         class1 = Class.forName("com.reflex.demo.Person");
         //由于这里不能带参数，所以你要实例化的这个类Person，一定要有无参构造函数哈～
@@ -108,14 +108,14 @@ public class Main {
 
     /**
      * Demo4: 通过Java反射机制得到一个类的构造函数，并实现创建带参实例对象
+     *
      * @throws ClassNotFoundException
      * @throws InvocationTargetException
      * @throws IllegalAccessException
      * @throws InstantiationException
      * @throws IllegalArgumentException
      */
-    public static void Demo4() throws ClassNotFoundException, IllegalArgumentException, InstantiationException, IllegalAccessException, InvocationTargetException
-    {
+    public static void Demo4() throws ClassNotFoundException, IllegalArgumentException, InstantiationException, IllegalAccessException, InvocationTargetException {
         Class<?> class1 = null;
         Person person1 = null;
         Person person2 = null;
@@ -128,7 +128,7 @@ public class Main {
         person1.setAge(30);
         person1.setName("leeFeng");
 
-        person2 = (Person) constructors[1].newInstance(20,"leeFeng");
+        person2 = (Person) constructors[1].newInstance(20, "leeFeng");
 
         System.out.println("Demo4: " + person1.getName() + " : " + person1.getAge()
                 + "  ,   " + person2.getName() + " : " + person2.getAge()
@@ -146,8 +146,7 @@ public class Main {
      * @throws InstantiationException
      * @throws ClassNotFoundException
      */
-    public static void Demo5() throws IllegalArgumentException, IllegalAccessException, SecurityException, NoSuchFieldException, InstantiationException, ClassNotFoundException
-    {
+    public static void Demo5() throws IllegalArgumentException, IllegalAccessException, SecurityException, NoSuchFieldException, InstantiationException, ClassNotFoundException {
         Class<?> class1 = null;
         class1 = Class.forName("com.reflex.demo.Person");
         Object obj = class1.newInstance();
@@ -165,15 +164,15 @@ public class Main {
 
     /**
      * Demo6: 通过Java反射机制得到类的一些属性： 继承的接口，父类，函数信息，成员信息，类型等
+     *
      * @throws ClassNotFoundException
      */
-    public static void Demo6() throws ClassNotFoundException
-    {
+    public static void Demo6() throws ClassNotFoundException {
         Class<?> class1 = null;
         class1 = Class.forName("com.reflex.demo.SuperMan");
 
         //取得父类名称
-        Class<?>  superClass = class1.getSuperclass();
+        Class<?> superClass = class1.getSuperclass();
         System.out.println("Demo6:  SuperMan类的父类名: " + superClass.getName());
 
         System.out.println("===============================================");
@@ -201,13 +200,14 @@ public class Main {
         //取得类实现的接口,因为接口类也属于Class,所以得到接口中的方法也是一样的方法得到哈
         Class<?> interfaces[] = class1.getInterfaces();
         for (int i = 0; i < interfaces.length; i++) {
-            System.out.println("实现的接口类名: " + interfaces[i].getName() );
+            System.out.println("实现的接口类名: " + interfaces[i].getName());
         }
 
     }
 
     /**
      * Demo7: 通过Java反射机制调用类方法
+     *
      * @throws ClassNotFoundException
      * @throws NoSuchMethodException
      * @throws SecurityException
@@ -216,8 +216,7 @@ public class Main {
      * @throws IllegalArgumentException
      * @throws InstantiationException
      */
-    public static void Demo7() throws ClassNotFoundException, SecurityException, NoSuchMethodException, IllegalArgumentException, IllegalAccessException, InvocationTargetException, InstantiationException
-    {
+    public static void Demo7() throws ClassNotFoundException, SecurityException, NoSuchMethodException, IllegalArgumentException, IllegalAccessException, InvocationTargetException, InstantiationException {
         Class<?> class1 = null;
         class1 = Class.forName("com.reflex.demo.SuperMan");
 
@@ -226,25 +225,24 @@ public class Main {
         method.invoke(class1.newInstance());
 
         System.out.println("调用有参方法walk(int m)：");
-        method = class1.getMethod("walk",int.class);
-        method.invoke(class1.newInstance(),100);
+        method = class1.getMethod("walk", int.class);
+        method.invoke(class1.newInstance(), 100);
     }
 
     /**
      * Demo8: 通过Java反射机制得到类加载器信息
-     *
+     * <p>
      * 在java中有三种类类加载器。[这段资料网上截取]
-
-     1）Bootstrap ClassLoader 此加载器采用c++编写，一般开发中很少见。
-
-     2）Extension ClassLoader 用来进行扩展类的加载，一般对应的是jre\lib\ext目录中的类
-
-     3）AppClassLoader 加载classpath指定的类，是最常用的加载器。同时也是java中默认的加载器。
+     * <p>
+     * 1）Bootstrap ClassLoader 此加载器采用c++编写，一般开发中很少见。
+     * <p>
+     * 2）Extension ClassLoader 用来进行扩展类的加载，一般对应的是jre\lib\ext目录中的类
+     * <p>
+     * 3）AppClassLoader 加载classpath指定的类，是最常用的加载器。同时也是java中默认的加载器。
      *
      * @throws ClassNotFoundException
      */
-    public static void Demo8() throws ClassNotFoundException
-    {
+    public static void Demo8() throws ClassNotFoundException {
         Class<?> class1 = null;
         class1 = Class.forName("com.reflex.demo.SuperMan");
         String nameString = class1.getClassLoader().getClass().getName();
@@ -253,20 +251,20 @@ public class Main {
     }
 
 
-
 }
+
 /**
- *
  * @author xiaoyaomeng
- *
  */
-class  Person{
+class Person {
     private int age;
     private String name;
-    public Person(){
+
+    public Person() {
 
     }
-    public Person(int age, String name){
+
+    public Person(int age, String name) {
         this.age = age;
         this.name = name;
     }
@@ -274,29 +272,31 @@ class  Person{
     public int getAge() {
         return age;
     }
+
     public void setAge(int age) {
         this.age = age;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
 }
 
-class SuperMan extends Person implements ActionInterface
-{
+class SuperMan extends Person implements ActionInterface {
     private boolean BlueBriefs;
 
-    public void fly()
-    {
+    public void fly() {
         System.out.println("超人会飞耶～～");
     }
 
     public boolean isBlueBriefs() {
         return BlueBriefs;
     }
+
     public void setBlueBriefs(boolean blueBriefs) {
         BlueBriefs = blueBriefs;
     }
@@ -307,6 +307,7 @@ class SuperMan extends Person implements ActionInterface
         System.out.println("超人会走耶～～走了" + m + "米就走不动了！");
     }
 }
+
 interface ActionInterface {
     public void walk(int m);
 }
