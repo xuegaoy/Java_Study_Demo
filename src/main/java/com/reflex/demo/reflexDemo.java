a4f6cf29ead1a5d3a6ab6f992403a99a0a5ea18a
xuegaoy <xuegaoyuan@aliyun.com>
2019-01-23 22:35:32 +0800

    update

--- src/main/java/com/reflex/demo/reflexDemo.java
+++ src/main/java/com/reflex/demo/reflexDemo.java
@@ -1,4 +1,4 @@
-package com.anson.jwee.demo.api;
+package com.reflex.demo;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
@@ -256,35 +256,35 @@ public class reflexDemo {
 /**
  * @author xiaoyaomeng
  */
-class Person {
-    private int age;
-    private String name;
-
-    public Person() {
-
-    }
-
-    public Person(int age, String name) {
-        this.age = age;
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-}
+//class Person {
+//    private int age;
+//    private String name;
+//
+//    public Person() {
+//
+//    }
+//
+//    public Person(int age, String name) {
+//        this.age = age;
+//        this.name = name;
+//    }
+//
+//    public int getAge() {
+//        return age;
+//    }
+//
+//    public void setAge(int age) {
+//        this.age = age;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//}
 
 class SuperMan extends Person implements ActionInterface {
     private boolean BlueBriefs;
