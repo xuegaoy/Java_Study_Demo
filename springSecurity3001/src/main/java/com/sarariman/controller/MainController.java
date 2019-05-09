9ec08229f6b541c11657472a5e40c3178a84837d
unknown <xuegaoyuan@aliyun.com>
2019-05-09 23:54:08 +0800

    login page self def

---
+++ springSecurity3001/src/main/java/com/sarariman/controller/MainController.java
@@ -0,0 +1,19 @@
+package com.sarariman.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 定义通用类
+ */
+@Controller
+public class MainController {
+    /**
+     * 登录页面
+     * @return
+     */
+    @RequestMapping("/userLogin")
+    public String login() {
+        return "login";
+    }
+}
